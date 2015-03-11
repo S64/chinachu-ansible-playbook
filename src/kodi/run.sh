aac5af89e6baa590255befe9d02e627babff3bd8
Shuma Yoshioka <s64.stdio@gmail.com>
2015-03-11 11:19:19 +0900

    Update

--- src/kodi/run.sh
+++ src/kodi/run.sh
@@ -5,14 +5,7 @@ pushd $CURRENT
 xhost +
 #docker-compose up --no-recreate
 
-docker run \
--it \
---privileged=true \
--v /tmp/.X11-unix:/tmp/.X11-unix \
--v ../../volumes/kodi:/root/volume \
---device /dev/snd:/dev/snd \
---name=kodi \
---detach=true
+docker run -it -d --privileged=true --name=kodi -v /tmp/.X11-unix:/tmp/.X11-unix -v ../../volumes/kodi:/root/volume --device /dev/snd:/dev/snd
 
 docker start -i kodi
 
