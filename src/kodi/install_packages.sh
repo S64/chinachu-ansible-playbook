6941d5af85f981948f91c256d4b354d2993d1652
Shuma Yoshioka <s64.stdio@gmail.com>
2015-03-11 19:14:44 +0900

    Update

---
+++ src/kodi/install_packages.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+yum -y install \
+ afpfs-ng-devel alsa-lib-devel autoconf autogen-libopts \
+ automake avahi-devel bluez-libs-devel boost-atomic boost-chrono \
+ boost-context boost-devel boost-filesystem boost-graph boost-iostreams \
+ boost-locale boost-math boost-program-options boost-python boost-random \
+ boost-regex boost-serialization boost-signals boost-test boost-timer \
+ boost-wave bzip2-devel cairo-devel cmake cpp dbus-devel enca-devel \
+ expat-devel faac-devel faad2-devel faad2-libs flac-devel \
+ fontconfig-devel freetype-devel fribidi-devel gcc gcc-c++ \
+ gettext-common-devel gettext-devel git glew-devel glib2-devel \
+ glibc-devel glibc-headers gl-manpages gnutls-c++ gnutls-devel gperf \
+ harfbuzz-devel jasper-devel java kernel-headers keyutils-libs-devel \
+ krb5-devel lame-devel lame-libs libass-devel libbluray-devel \
+ libcap-devel libcdio-devel libcom_err-devel libcrystalhd-devel libcurl-devel \
+ libdca-devel libdrm-devel libgcrypt-devel libGLEW libGLEWmx \
+ libgpg-error-devel libICE-devel libicu-devel libjpeg-turbo-devel \
+ libmad-devel libmicrohttpd-devel libmikmod libmms-devel \
+ libmodplug-devel libmp4v2 libmpc libmpeg2-devel libogg-devel \
+ libplist-devel libpng-devel librtmp-devel \
+ libsamplerate-devel libselinux-devel libsepol-devel libsmbclient-devel \
+ libSM-devel libssh-devel libstdc++-devel libtasn1-devel \
+ libtiff-devel libtool libusb-devel libusbx-devel libva-devel \
+ libvdpau-devel libverto-devel libvorbis-devel libX11-devel libXau-devel \
+ libxcb-devel libXdamage-devel libXext-devel libXfixes-devel libXft-devel \
+ libXi-devel libXinerama-devel libxml2-devel libXmu-devel libXrandr-devel \
+ libXrender-devel libXScrnSaver-devel libXt-devel libXtst-devel \
+ libXxf86vm-devel lzma-sdk457 lzo-devel lzo-minilzo m4 mariadb \
+ mariadb-devel mariadb-libs mesa-libEGL-devel mesa-libGL-devel \
+ mesa-libGLES-devel mesa-libGLU-devel openssl-devel \
+ p11-kit-devel pango-devel patch pcre-devel perl-autodie perl-B-Lint \
+ perl-Business-ISBN perl-Business-ISBN-Data perl-CGI perl-Class-ISA \
+ perl-Compress-Raw-Bzip2 perl-Compress-Raw-Zlib perl-CPAN perl-Data-Dumper \
+ perl-devel perl-Digest perl-Digest-MD5 perl-Digest-SHA perl-Encode-Locale \
+ perl-Error perl-ExtUtils-Install perl-ExtUtils-MakeMaker \
+ perl-ExtUtils-Manifest perl-ExtUtils-ParseXS perl-FCGI perl-File-CheckTree \
+ perl-File-Listing perl-Git perl-HTML-Parser perl-HTML-Tagset \
+ perl-HTTP-Cookies perl-HTTP-Daemon perl-HTTP-Date perl-HTTP-Message \
+ perl-HTTP-Negotiate perl-HTTP-Tiny perl-IO-Compress perl-IO-HTML \
+ perl-IO-Socket-IP perl-IO-Socket-SSL perl-libwww-perl perl-Locale-Codes \
+ perl-LWP-MediaTypes perl-Module-Pluggable perl-Net-HTTP perl-Net-LibIDN \
+ perl-Net-SSLeay perl-parent perl-Pod-Checker perl-Pod-LaTeX perl-podlators \
+ perl-Pod-Parser perl-Pod-Perldoc perl-Pod-Plainer perl-Pod-Usage \
+ perl-Sys-Syslog perl-TermReadKey perl-Test-Harness perl-Test-Simple \
+ perl-Text-ParseWords perl-Text-Soundex perl-Text-Unidecode perl-Thread-Queue \
+ perl-TimeDate perl-URI perl-WWW-RobotRules perl-XML-LibXML \
+ perl-XML-NamespaceSupport perl-XML-SAX perl-XML-SAX-Base physfs pixman-devel \
+ python-devel redhat-lsb redhat-lsb-core redhat-lsb-cxx redhat-lsb-desktop \
+ redhat-lsb-languages redhat-lsb-printing redhat-lsb-submod-multimedia \
+ redhat-lsb-submod-security SDL2-devel SDL2_image-devel \
+ SDL-devel SDL_gfx-devel SDL_image-devel SDL_mixer-devel SDL_net-devel \
+ SDL_Pango-devel SDL_sound-devel SDL-static SDL_ttf-devel \
+ spax sqlite-devel swig systemtap-sdt-devel taglib-devel \
+ tinyxml-devel tre-devel vala wavpack-devel xorg-x11-proto-devel \
+ xz-devel yajl-devel zlib-devel \
+ libxslt libxslt-devel yasm yasm-devel trousers trousers-devel doxygen
