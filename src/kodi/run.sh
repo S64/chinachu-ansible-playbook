7d6546424aa9a7bb51401118907acf93943b23c8
Shuma Yoshioka <s64.stdio@gmail.com>
2015-03-11 11:16:46 +0900

    Update

--- src/kodi/run.sh
+++ src/kodi/run.sh
@@ -3,6 +3,17 @@ CURRENT=`dirname $0`
 pushd $CURRENT
 
 xhost +
-docker-compose up --no-recreate
+#docker-compose up --no-recreate
+
+docker run \
+-it \
+--privileged=true \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-v ../../volumes/kodi:/root/volume \
+--device /dev/snd:/dev/snd \
+--name=kodi \
+--detach=true
+
+docker start -i kodi
 
 popd
